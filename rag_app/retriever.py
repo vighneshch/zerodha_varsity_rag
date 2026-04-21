4792b1c7b69f67700bf265a58d61f9f0edbce166
Vighnesh Challa <challavighnesh@hotmail.co.in>
2026-04-21 11:39:53 +0530

    Added get_retreiever

---
+++ rag_app/retriever.py
@@ -0,0 +1,19 @@
+# Loading the required libraries
+from app.config import config
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+
+def get_retriever():
+    qdrant_url = config['qdrant']['url']
+    qdrant_collection_name = config['qdrant']['collection_name']
+    embeddings = HuggingFaceEmbeddings(model=config['model']['embedding_model'])
+
+    client = QdrantClient(url=qdrant_url)
+
+    vector_db  = QdrantVectorStore(client=client,
+                                   collection_name=qdrant_collection_name,
+                                   embedding=embeddings)
+    retreiver = vector_db.as_retriever(search_kwargs={"k":3})
+
+    return retreiver
