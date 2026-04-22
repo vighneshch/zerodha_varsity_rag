3f0aa33028f1ab6cffaf017efd5f3294f0243c15
Vighnesh Challa <challavighnesh@hotmail.co.in>
2026-04-22 19:34:59 +0530

    Optimized the retreiving speed

--- rag_app/pipeline.py
+++ rag_app/pipeline.py
@@ -1,10 +1,9 @@
 # Loading the required librarbeis
 from rag_app.retriever import get_retriever
 from rag_app.generator import get_answer
+
 retriever = get_retriever()
 def answer(query):
-    
     docs = retriever.invoke(query)
-    answer = get_answer(docs,query)
-
-    return answer
+    answer_content = get_answer(docs,query)
+    return answer_content
